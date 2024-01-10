0967588b321642f3f090c7c302d6548e78df2c8e
florianPalabost <palabostf@gmail.com>
2024-01-10 16:49:50 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    import { RouterView } from 'vue-router';
+    import AppHeader from '@/components/layout/AppHeader.vue';
+</script>
+
+<template>
+    <AppHeader />
+    <div class="container mx-auto">
+        <suspense>
+            <RouterView />
+        </suspense>
+    </div>
+</template>
+
+<style scoped></style>
